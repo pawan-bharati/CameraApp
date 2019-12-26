7e252cd4d8657d36a44af19c0307bfd965e99034
pawan_bharati <pawanbharati90@gmail.com>
2019-12-26 08:04:06 +0545

    gallery

--- app/src/main/java/com/example/cameraapp/Gallery.java
+++ app/src/main/java/com/example/cameraapp/Gallery.java
@@ -1,14 +1,68 @@
 package com.example.cameraapp;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class Gallery extends AppCompatActivity {
+    ImageView imageView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+
+        imageView2=findViewById(R.id.imageView2);
+
+        imageView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                BrowseImage();
+            }
+        });
+    }
+    private void BrowseImage(){
+        Intent intent = new Intent(Intent.ACTION_PICK);
+        intent.setType("image/*");
+        startActivityForResult(intent,0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode== RESULT_OK){
+            if(data==null){
+                Toast.makeText(this, "please select an image ", Toast.LENGTH_SHORT).show();
+            }
+        }
+        Uri uri =data.getData();
+        imageView2.setImageURI(uri);
+
+        String imagePath = getRealPathFromUri(uri);
+        Toast.makeText(this, "" +imagePath, Toast.LENGTH_SHORT).show();
+    }
+
+    private String getRealPathFromUri(Uri uri){
+        String[] projection = {MediaStore.Images.Media.DATA};
+        CursorLoader loader = new CursorLoader(getApplicationContext(),uri, projection, null,null,null);
+        Cursor cursor = loader.loadInBackground();
+        int colIndex =cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        String result = cursor.getString(colIndex);
+        cursor.close();
+        return result;
     }
 }
+
